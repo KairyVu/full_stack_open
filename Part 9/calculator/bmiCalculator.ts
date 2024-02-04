03770a8c423554d6d2f3b81db0e5381c21eca590
Kairy Vu <kairyvd167@gmail.com>
2024-02-04 00:27:13 -0500

    bmiCalculator.ts exercise 9.1

--- Part 9/calculator/bmiCalculator.ts
+++ Part 9/calculator/bmiCalculator.ts
@@ -1,3 +1,21 @@
+interface CalculateBmi {
+  height: number;
+  weight: number;
+}
+
+const parseArguments = (args: string[]): CalculateBmi => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+  if (args.length > 4) throw new Error("Too many arguments");
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3]),
+    };
+  }
+  throw new Error("Provided values were not numbers!");
+};
+
 const calculateBmi = (h: number, w: number): string => {
   const bmi = w / (h / 100) ** 2;
   switch (true) {
@@ -20,4 +38,13 @@ const calculateBmi = (h: number, w: number): string => {
   }
 };
 
-console.log(calculateBmi(180, 74));
+try {
+  const { height, weight } = parseArguments(process.argv);
+  console.log(calculateBmi(height, weight));
+} catch (error: unknown) {
+  let errorMessage = "Something bad happened.";
+  if (error instanceof Error) {
+    errorMessage += " Error: " + error.message;
+  }
+  console.log(errorMessage);
+}
