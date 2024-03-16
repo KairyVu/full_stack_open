557f02b5519425543622b6a7733c68be80f92c6f
Kairy Vu <kairyvd167@gmail.com>
2024-03-16 11:25:47 -0400

    submit 2.11

--- Part 2/phonebook/src/App.jsx
+++ Part 2/phonebook/src/App.jsx
@@ -1,91 +1,103 @@
-import {useState} from 'react'
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-const Filter = ({val, onChange}) => (
+const Filter = ({ val, onChange }) => (
   <div>
     filter shown with <input value={val} onChange={onChange} />
   </div>
-)
+);
 
 const PersonForm = (props) => (
   <form onSubmit={props.submit}>
-    <div>name: <input value={props.name} onChange={props.nameChange}/></div>
-    <div>number: <input value={props.number} onChange={props.numberChange}/></div>
+    <div>
+      name: <input value={props.name} onChange={props.nameChange} />
+    </div>
+    <div>
+      number: <input value={props.number} onChange={props.numberChange} />
+    </div>
     <div>
       <button type="submit">add</button>
     </div>
   </form>
-)
+);
 
-const Persons = ({persons, onDisplay}) => (
+const Persons = ({ persons, onDisplay }) => (
   <>
-  {persons.filter((person) => (onDisplay(person))).map((person) =>
-    <div key={person.id}>{person.name} {person.number}</div>
-  )}
+    {persons
+      .filter((person) => onDisplay(person))
+      .map((person) => (
+        <div key={person.id}>
+          {person.name} {person.number}
+        </div>
+      ))}
   </>
-)
-
+);
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [newSearch, setNewSearch] = useState('')
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [newSearch, setNewSearch] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => setPersons(response.data));
+  });
 
   const addName = (event) => {
-    event.preventDefault()
-    let state = false
-    persons.forEach((person) =>
-      state = (state || person.name === newName)
-    )
+    event.preventDefault();
+    let state = false;
+    persons.forEach((person) => (state = state || person.name === newName));
     if (!state) {
       const newNameAdd = {
         name: newName,
         number: newNumber,
-        id: persons.length + 1
-      }
-      setPersons(persons.concat(newNameAdd))
+        id: persons.length + 1,
+      };
+      setPersons(persons.concat(newNameAdd));
+    } else {
+      alert(`${newName} is already added to phonebook`);
     }
-    else {
-      alert(`${newName} is already added to phonebook`)
-    }
-    setNewName('')
-    setNewNumber('')
-  }
+    setNewName("");
+    setNewNumber("");
+  };
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
 
   const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+    setNewNumber(event.target.value);
+  };
 
   const handleSearch = (event) => {
-    setNewSearch(event.target.value)
-  }
+    setNewSearch(event.target.value);
+  };
 
   const onHandleSearch = (person) => {
-    return person.name.toLowerCase().includes(newSearch.toLowerCase())
-  }
+    return person.name.toLowerCase().includes(newSearch.toLowerCase());
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
       <Filter value={newSearch} onChange={handleSearch} />
       <h3>Add a new</h3>
-      
-      <PersonForm submit={addName} name={newName} nameChange={handleNameChange} number={newNumber} numberChange={handleNumberChange} />
+
+      <PersonForm
+        submit={addName}
+        name={newName}
+        nameChange={handleNameChange}
+        number={newNumber}
+        numberChange={handleNumberChange}
+      />
 
       <h3>Numbers</h3>
-      
+
       <Persons persons={persons} onDisplay={onHandleSearch} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
