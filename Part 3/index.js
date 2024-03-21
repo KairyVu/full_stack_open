d5be544b985f2fd62987b8edf79cc91fe7699145
Kairy Vu <kairyvd167@gmail.com>
2024-03-21 10:47:15 -0400

    submit 3.2

--- Part 3/index.js
+++ Part 3/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-let notes = [
+let persons = [
   {
     id: 1,
     name: "Arto Hellas",
@@ -25,7 +25,15 @@ let notes = [
 ];
 
 app.get("/api/persons", (request, response) => {
-  response.json(notes);
+  response.json(persons);
+});
+
+app.get("/info", (request, response) => {
+  const date = new Date();
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`
+  );
+  console.log();
 });
 
 const PORT = 3001;
