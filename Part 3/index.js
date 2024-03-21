bd65be766721aef6614edad6ca63f86fe4c637e5
Kairy Vu <kairyvd167@gmail.com>
2024-03-21 17:37:33 -0400

    submit 3.5

--- Part 3/index.js
+++ Part 3/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 let persons = [
   {
@@ -51,6 +52,26 @@ app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
   persons = persons.filter((per) => per.id !== id);
   response.status(204).end();
+});
+
+const generateId = () => {
+  return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body) {
+    return response.status(400).json({ error: "content missing" });
+  }
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+  response.json(person);
 });
 
 const PORT = 3001;
